9293bf5b6f5a929fe1a6d92bdb5a0f35ff761348
Carl <ctrand@gmail.com>
2012-11-06 00:21:50 +1100

    Switched to production urls

--- lib/omniauth/strategies/authic.rb
+++ lib/omniauth/strategies/authic.rb
@@ -25,10 +25,16 @@ module OmniAuth
         }
       end
       
+      def request_phase
+        # Add authic_action into the params mix. This will let the Authic server know to serve up the signup or signin page
+        options.authorize_params[:authic_action] = request.params["authic_action"]
+        super
+      end
+      
       def client
         raise "You must specify your Authic subdomain in setup i.e. :subdomain => 'mysubdomain'" if options[:subdomain].blank?
         # Make sure we set the site correctly before creating a client
-        options[:client_options][:site] = "http://#{options[:subdomain]}.#{options[:domain]}"
+        options[:client_options][:site] = "https://#{options[:subdomain]}.#{options[:domain]}"
         super
       end
       
